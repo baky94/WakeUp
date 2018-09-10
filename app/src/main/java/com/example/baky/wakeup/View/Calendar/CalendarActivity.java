a37d12c6086b92e09d8f62214e10d36251db626a
hyuno0 <khw3242@naver.com>
2018-09-10 21:24:25 +0900

    달력 프래그먼트 추가

--- app/src/main/java/com/example/baky/wakeup/View/Calendar/CalendarActivity.java
+++ app/src/main/java/com/example/baky/wakeup/View/Calendar/CalendarActivity.java
@@ -1,22 +1,22 @@
-package com.example.baky.wakeup.View.Fragment;
+package com.example.baky.wakeup.View.Calendar;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.GridView;
 import android.widget.TextView;
 
 import com.example.baky.wakeup.R;
-import com.example.baky.wakeup.View.Calendar.MonthAdapter;
-import com.example.baky.wakeup.View.Calendar.MonthItem;
-import com.example.baky.wakeup.View.MainActivity;
 
-public class CalendarTab extends Fragment {
+/**
+ * 그리드뷰를 이용해 월별 캘린더를 만드는 방법에 대해 알 수 있습니다.
+ *
+ * @author Mike
+ */
+public class CalendarActivity extends AppCompatActivity {
 
     /**
      * 월별 캘린더 뷰 객체
@@ -42,18 +42,15 @@ public class CalendarTab extends Fragment {
      * 현재 월
      */
     int curMonth;
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_calendar_tab, container, false);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
         // 월별 캘린더 뷰 객체 참조
-        monthView = (GridView) view.findViewById(R.id.monthView);
-        monthViewAdapter = new MonthAdapter(getContext());
+        monthView = (GridView) findViewById(R.id.monthView);
+        monthViewAdapter = new MonthAdapter(this);
         monthView.setAdapter(monthViewAdapter);
 
         // 리스너 설정
@@ -69,11 +66,11 @@ public class CalendarTab extends Fragment {
         });
 
 
-        monthText = (TextView) view.findViewById(R.id.monthText);
+        monthText = (TextView) findViewById(R.id.monthText);
         setMonthText();
 
         // 이전 월로 넘어가는 이벤트 처리
-        Button monthPrevious = (Button) view.findViewById(R.id.monthPrevious);
+        Button monthPrevious = (Button) findViewById(R.id.monthPrevious);
         monthPrevious.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 monthViewAdapter.setPreviousMonth();
@@ -84,7 +81,7 @@ public class CalendarTab extends Fragment {
         });
 
         // 다음 월로 넘어가는 이벤트 처리
-        Button monthNext = (Button) view.findViewById(R.id.monthNext);
+        Button monthNext = (Button) findViewById(R.id.monthNext);
         monthNext.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 monthViewAdapter.setNextMonth();
@@ -94,16 +91,16 @@ public class CalendarTab extends Fragment {
             }
         });
 
-        return view;
     }
 
     /**
      * 월 표시 텍스트 설정
      */
-    public void setMonthText() {
+    private void setMonthText() {
         curYear = monthViewAdapter.getCurYear();
         curMonth = monthViewAdapter.getCurMonth();
 
         monthText.setText(curYear + "년 " + (curMonth + 1) + "월");
     }
+
 }
