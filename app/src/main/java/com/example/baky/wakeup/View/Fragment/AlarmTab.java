3c91ddf664d8c978e9e7dc0d222ce444619e9d04
jgh6512 <42927457+jgh6512@users.noreply.github.com>
2018-09-10 23:49:47 +0900

    alarmTab xml + spinner(part of it)

--- app/src/main/java/com/example/baky/wakeup/View/Fragment/AlarmTab.java
+++ app/src/main/java/com/example/baky/wakeup/View/Fragment/AlarmTab.java
@@ -5,6 +5,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.example.baky.wakeup.R;
 
@@ -12,10 +16,24 @@ public class AlarmTab extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_alarm_tab,container,false);
+        Spinner s = (Spinner)view.findViewById(R.id.spinner);
+        s.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+                tv.setText("position : " + position +
+                        parent.getItemAtPosition(position));
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {}
+        });
+
         return inflater.inflate(R.layout.fragment_alarm_tab, container, false);
     }
 }
